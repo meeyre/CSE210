5d03bffde016a37eec3db2cf2725326ba6d05931
meeyre <eyr21002@byui.edu>
2025-04-26 15:12:28 -0600

    Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade precentage: ");
+        int x = int.Parse(Console.ReadLine());
+        string letter;
+
+        if (x>=90)
+        {
+            letter = "A";
+        }
+        else if (x>=80)
+        {
+            letter = "B";
+        }
+        else if (x>=70)
+        {
+            letter = "C";
+        }
+        else if (x>=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade was \"{letter}\"");
+        if (x>=70)
+        {
+            Console.WriteLine("You passed! Great job!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, better luck next time!");
+        }
     }
 }
