ce66f290e79e153a04397df4657f861e4306bc9a
meeyre <eyr21002@byui.edu>
2025-04-26 18:04:42 -0600

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+    Console.WriteLine("Welcome to the program!");
+    }
+    static string GetName()
+    {
+    Console.Write("Please input your name: ");
+    return Console.ReadLine();
+    }
+    static int GetNumber()
+    {
+    Console.Write("Please input your favorite number: ");
+    return int.Parse(Console.ReadLine());
+    }
+    static int SquareUserNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayData(string name, int number)
+    {
+    
+    Console.WriteLine($"{name}, the square of your favorite number is {SquareUserNumber(number)}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayData(GetName(), GetNumber());
     }
 }
