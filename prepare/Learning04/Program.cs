15de305f3017706c6bf99d5aea865d778ae46f41
meeyre <eyr21002@byui.edu>
2025-05-05 12:42:46 -0500

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,50 @@
 using System;
+class Assignment
+{
+    private string _studentName;
+    private string _topic;
+    public Assignment(string name, string topic){
+        _studentName = name;
+        _topic = topic;
+    }
+    public string GetSummary(){
+        return $"{_studentName} - {_topic}";
+    }
+    public string GetName(){
+        return _studentName;
+    }
+}
 
+class WritingAssignment : Assignment
+{   
+    private string _title;
+    public WritingAssignment(string name, string topic , string title) : base(name, topic){
+        _title = title;
+    }
+    public string GetWritingInfo(){
+        return $"{GetSummary()}\n{_title} by {GetName()}";
+    }
+}
+
+class MathAssignment : Assignment
+{   
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string name, string topic, string textbookSection, string problems) : base(name, topic){
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetHomework(){
+        return $"{GetSummary()}\n{_textbookSection} : {_problems}";
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        WritingAssignment writingHW = new WritingAssignment("Bob", "European History", "Mideaval Castles");
+        MathAssignment mathHW = new MathAssignment("Bill", "Fractions", "7.3-7.4", "45-47");
+        Console.WriteLine(writingHW.GetWritingInfo());
+        Console.WriteLine(mathHW.GetHomework());
     }
 }
